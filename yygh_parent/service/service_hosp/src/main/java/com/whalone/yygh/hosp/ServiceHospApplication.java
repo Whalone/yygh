31cb237f32dda03beee44d326254eeaccdab5729
Whalone <469716638@qq.com>
2021-03-31 18:54:57 +0800

    run swagger

--- yygh_parent/service/service_hosp/src/main/java/com/whalone/yygh/hosp/ServiceHospApplication.java
+++ yygh_parent/service/service_hosp/src/main/java/com/whalone/yygh/hosp/ServiceHospApplication.java
@@ -1,9 +1,13 @@
 package com.whalone.yygh.hosp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.whalone")
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
--- yygh_parent/service/service_hosp/src/main/java/com/whalone/yygh/hosp/controller/HospitalSetController.java
+++ yygh_parent/service/service_hosp/src/main/java/com/whalone/yygh/hosp/controller/HospitalSetController.java
@@ -3,9 +3,7 @@ package com.whalone.yygh.hosp.controller;
 import com.whalone.yygh.hosp.service.HospitalSetService;
 import com.whalone.yygh.model.hosp.HospitalSet;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,10 +16,18 @@ public class HospitalSetController {
 
     // http://localhost:8201/admin/hosp/hospitalSet/findAll
 
+    // 查询医院设置表的所有信息
     @GetMapping("findAll")
     public List<HospitalSet> findAllHospitalSet(){
         List<HospitalSet> list = hospitalSetService.list();
         return list;
+    }
+
+    // 2 逻辑删除医院设置
+    @DeleteMapping("{id}")
+    public boolean removeHospSet(@PathVariable Long id){
+        boolean flag = hospitalSetService.removeById(id);
+        return flag;
     }
 
 }
